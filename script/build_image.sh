e75a7ab7d0294f72647be5e5d5ea6e3469b02f25
iwauo <iwauo.tajima@gmail.com>
2019-08-19 13:16:30 +0900

    install node runtimes via nvm

--- script/build_image.sh
+++ script/build_image.sh
@@ -9,5 +9,6 @@ set -x
 docker build ../image_context \
   --build-arg CODE_SERVER_VERSION=$CODE_SERVER_VERSION \
   --build-arg SDKMAN_LIBS=$SDKMAN_LIBS \
+  --build-arg NODE_VERSIONS=$NODE_VERSIONS \
   --build-arg VSCODE_EXTENSIONS="$VSCODE_EXTENSIONS" \
   -t $CURRENT
