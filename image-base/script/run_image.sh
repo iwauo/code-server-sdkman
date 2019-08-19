015e946843cb820cca2e4684e28dc0cab83a2e2c
iwauo <iwauo.tajima@gmail.com>
2019-08-19 16:56:56 +0900

    generate a predefined bashrc

--- image-base/script/run_image.sh
+++ image-base/script/run_image.sh
@@ -3,7 +3,11 @@ set -e
 source ./common_settings.sh
 
 set -x
+
+docker rm -f $IMAGE_NAME || true
+
 docker run \
+  --name $IMAGE_NAME \
   -it -p 8443:8443 \
   -v "${PWD}:/home/developer/project" \
   $CURRENT \
--- image-jdk8/script/run_image.sh
+++ image-jdk8/script/run_image.sh
@@ -1,15 +1,15 @@
 #!/usr/bin/env bash
 set -e
 source ./common_settings.sh
-NAME=code-server-sdkman-kotlin
 
 set -x
-docker rm -f $NAME || true
+docker rm -f $IMAGE_NAME || true
 docker run \
-  --name $NAME \
+  --rm \
+  --name $IMAGE_NAME \
   -it -p 8443:8443 \
   -v "${PWD}:/home/developer/project:delegated" \
   $CURRENT \
   --allow-http \
-  --no-auth \
+  --no-auth
 
