ef884c548738fa22296df440f0857dc37dc77cf9
iwauo <iwauo.tajima@gmail.com>
2019-10-25 19:29:05 +0900

    update gradle/jdk

--- image-jdk8/script/run_image.sh
+++ image-jdk8/script/run_image.sh
@@ -7,9 +7,10 @@ docker rm -f $IMAGE_NAME || true
 docker run \
   --rm \
   --name $IMAGE_NAME \
-  -it -p 8000:8080 \
+  -it \
+  -p 8080:8080 \
   -v "${PWD}:/home/developer/project:delegated" \
   $CURRENT \
   --allow-http \
-  --no-auth
+  --auth none
 
